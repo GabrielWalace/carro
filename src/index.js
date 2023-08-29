543c9e54f7efaf73f875201fe4eafcc3ed3124b2
Suyndara <souzamatheussilva41@gmail.com>
2023-08-29 15:09:59 -0300

    aaaa

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 
